5b730840562802db9abe87a66beae90d16ae6f1c
sharayuanuse <sharayu.anuse@gmail.com>
2024-07-24 20:05:28 +0530

    Implement Register and Login functionality

---
+++ public/src/pages/SetAvatar.jsx
@@ -0,0 +1,17 @@
+import React , {useEffect,useState} from 'react'
+import styled from 'styled-components'
+import {useNavigate} from 'react-router-dom'
+import loader from '../assets/loader.svg'
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import axios from 'axios'
+import { SetAvatarRoute } from '../utils/APIRoutes'
+
+export default function SetAvatar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
